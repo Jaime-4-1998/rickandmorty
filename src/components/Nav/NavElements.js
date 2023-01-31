ee5cbcb28c856570574103923f0a6aaf8dada063
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-01-31 17:59:44 -0600

    Added responsive sdesing

--- src/components/Nav/NavElements.js
+++ src/components/Nav/NavElements.js
@@ -9,9 +9,17 @@ export const NavAtributtes = styled.div`
     flex-direction:row;
     gap:3rem;
     justify-content: center;
+    @media screen and (max-width:900px){
+        flex-direction:column;
+        gap:0.5rem;
+    }
 `;
 export const NavParr = styled.a`
     color:#fff;
     padding:2rem;
     text-decoration:none;
+    @media screen and (max-width:900px){
+        padding:1.5rem;
+        text-align:center;
+    }
 `;
