f21c54c14f1072d1b25fb388da9ce688ad047b6f
Jaime-4-1998 <jordan230-jaime@hotmail.com>
2023-01-31 15:17:36 -0600

    Added the desing in the form of seach

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-
+import './desing.css'
 const index = ({ setSearch }) => {
   const searchBtn = (e) => {
     e.preventDefault();
   };
   return (
-    <form>
-    <input
-      onChange={(e) => {
-        setSearch(e.target.value);
-      }}
-      placeholder="Buscar Personajes"
-      type="text"
-    />
-    <button
-      onClick={searchBtn}
-    >
-      Search
-    </button>
-  </form>
+    //form to find the character
+    <section>
+      <div className="search__api__cont">
+        <form>
+          <input
+            className='input__api__search'
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            placeholder="Buscar Personajes"
+            type="text"
+          />
+        <button onClick={searchBtn} className='btn__api'>Buscar</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
